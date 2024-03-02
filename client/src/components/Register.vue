67bd6a96f0e72e7f7cb7b41d662eaabb37f31308
solo-leveler <shinichiizumi@protonmail.ch>
2024-03-02 12:22:15 +0530

    register component added with route

---
+++ client/src/components/Register.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1> Register</h1>
+        <input type="email" v-model="email" name="email" id="email" placeholder="email">
+        <br>
+        <input type="password" v-model="password" name="password" id="password" placeholder="password">
+        <br>
+        <button type="submit">Register</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
