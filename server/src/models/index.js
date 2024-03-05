1cb6533ed1706e2414395f41efc72f3b44b82484
solo-leveler <shinichiizumi@protonmail.ch>
2024-03-05 22:56:20 +0530

    fix : corrected model path

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -14,11 +14,12 @@ const sequelize = new Sequelize(
 
 fs
     .readdirSync(__dirname)
-    .filter(file => 
+    .filter((file) => 
         file !== 'index.js'
     )
     .forEach((file) => {
-        const model = sequelize.import(path.join(__dirname,file))
+        //const model = sequelize.import(path.join(__dirname,file))
+        const model = require(path.join(__dirname,file))(sequelize, Sequelize.DataTypes)
         db[model.name] = model 
     })
 
