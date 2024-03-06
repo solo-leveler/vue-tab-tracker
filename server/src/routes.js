d5ec01843bc195371b6016af1ddab6e0697df64b
solo-leveler <shinichiizumi@protonmail.ch>
2024-03-06 22:08:49 +0530

    enhancement : routed to authenticationcontroller

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,6 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
 module.exports = (app) => {
-  app.get('/status', (req, res) => {
-    res.send({
-      message: 'Hello World!'
-    })
-  })
+  app.post('/register', AuthenticationController.register
+  )
 }
