eaea0ec06dc1382fc7ee3f2ce60efe4ae3dbe242
solo-leveler <shinichiizumi@protonmail.ch>
2024-04-16 09:10:28 +0530

    adding toolbar items

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -3,6 +3,17 @@
         <v-toolbar-title class="mr-4">
             TabTracker
         </v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn flat dark>
+                Browse
+            </v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+            <v-btn flat dark>
+                Sign Up
+            </v-btn>
+        </v-toolbar-items>
     </v-toolbar>
 </template>
 <script>
