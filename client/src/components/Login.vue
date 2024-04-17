f8d73a4a2191b91e25aeac08c22f431271dae595
solo-leveler <shinichiizumi@protonmail.ch>
2024-04-17 23:54:52 +0530

    added login file and service

---
+++ client/src/components/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container fluid column>
+    <v-flex xs6 offset-x3>
+      <div class="white-elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>login</v-toolbar-title>
+        </v-toolbar>
+      </div>
+      <div class="px-4 py-2">
+        <form
+          name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <br>
+        <div class="danger-alert" v-html="error" />
+        <br>
+        <v-btn
+          dark
+          class="cyan"
+          @click="login">
+          Login
+        </v-btn>
+      </div>
+    </v-flex>
+  </v-container>
+</template>
+
+<script>
+import AuthencationService from '../services/AuthencationService'
+export default {
+  name: 'login',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        AuthencationService.login({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.error {
+  color : red;
+}
+</style>
