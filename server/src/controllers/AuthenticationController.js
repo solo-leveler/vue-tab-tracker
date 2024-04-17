1ef91c7fc21543901403b9a53f6cbc121c6f455a
solo-leveler <shinichiizumi@protonmail.ch>
2024-04-17 23:56:45 +0530

    added login controller

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,17 +1,47 @@
 const {User} = require('../models')
 
 module.exports = {
-    async register (req, res) {
-        try {
-            const user = await User.create(req.body);         
-            res.status(200).send(
-                user.toJSON()
-            )
-        } catch (error) {
-            //user or email already exists
-            res.status(400).send({
-                error : 'This email account is already in use'
-            })
+  async register (req, res) {
+    try {
+      const user = await User.create(req.body)
+      res.status(200).send(
+        user.toJSON()
+      )
+    } catch (error) {
+      // user or email already exists
+      res.status(400).send({
+        error: 'This email account is already in use'
+      })
+    }
+  },
+  async login (req, res) {
+    try {
+      const {email, password} = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
         }
+      })
+      if (!user) {
+        res.status(403).send({
+          error: 'The login information was incorrect'
+        })
       }
+      const isPasswordValid = password === user.password
+      if (!isPasswordValid) {
+        res.status(403).send({
+          error: 'The password information was incorrect'
+        })
+      }
+      res.status(200).send(
+        {user: user.toJSON()}
+      )
+    } catch (error) {
+      // user or email already exists
+      res.status(500).send({
+        error: 'Error trying to login'
+      })
+    }
+  }
+
 }
