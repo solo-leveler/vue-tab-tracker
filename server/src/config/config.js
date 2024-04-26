91c50aa5a2da608d6fdc12fab09949b61bd087f3
solo-leveler <shinichiizumi@protonmail.ch>
2024-04-26 22:05:15 +0530

    edited config for login

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    port : process.env.PORT || 8081,
-    db : {
-        database : process.env.DB_NAME || 'tabtracker',
-        user : process.env.DB_USER || 'tabtracker',
-        password : process.env.DB_PASS || 'tabtracker',
-        options : {
-            dialect : process.env.DB_DIALECT || 'sqlite',
-            host : process.env.HOST || 'localhost',
-            storage : 'tabtracker.sqlite',
-        }
-
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'tabtracker',
+    user: process.env.DB_USER || 'tabtracker',
+    password: process.env.DB_PASS || 'tabtracker',
+    options: {
+      dialect: process.env.DB_DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: 'tabtracker.sqlite'
     }
+
+  },
+  authencation: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
+
+  }
 }
