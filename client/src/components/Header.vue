bc4e87629493b8b03a24b1182ccabd3bc211db36
solo-leveler <shinichiizumi@protonmail.ch>
2024-04-28 11:32:36 +0530

    added logout button & logic

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -18,11 +18,23 @@
             <v-btn v-if="!$store.state.isUserLoggedIn" flat dark to="register">
                 Sign Up
             </v-btn>
+            <v-btn v-if="$store.state.isUserLoggedIn" flat dark @click="logout">
+                Logout
+            </v-btn>
         </v-toolbar-items>
     </v-toolbar>
 </template>
 <script>
 export default{
+  methods: {
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
+    }
+  }
 
 }
 </script>
