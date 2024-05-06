7a99a3d5f165be6df5f587d9f3406af8ac241414
solo-leveler <shinichiizumi@protonmail.ch>
2024-05-06 23:11:39 +0530

    code-enhancement : style changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,12 +2,9 @@
   <div id="app">
     <v-app>
         <page-header />
-
-      <main>
         <v-container fluid>
           <router-view></router-view>
         </v-container>
-      </main>
     </v-app>
   </div>
 </template>
